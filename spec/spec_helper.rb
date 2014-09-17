a00751401e89ca61cc7030ded93ba99b0ff990e2
aekagakea <elizabeth_steckenrid@taylor.edu>
2014-09-17 04:30:53 +0000

    add spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require 'rspec/rails'
 require 'rspec/autorun'
 
 require 'capybara/rspec'
+require 'rails_helper'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
